23058b70c6532d5a9c0c2241f717234e6c7e1098
Khoi Nguyen <pnknguyen0211@gmail.com>
2024-11-05 10:59:25 +0700

    solved p58_length_of_last_word

---
+++ src/leetcode/easy/p58_length_of_last_word.java
@@ -0,0 +1,28 @@
+public class p58_length_of_last_word {
+
+    /*
+     * Runtime 0 ms Beats 100%
+     */
+    public static int length_of_last_word(String s) {
+        s = s.trim();
+        int n = s.length();
+        int i = n - 1;
+        while (i >= 0 && s.charAt(i) != ' ') {
+            i--;
+        }
+        return n - i - 1;
+    }
+
+    /*
+     * Runtime 2 ms Beats 11.61%
+     */
+    public static int length_of_last_word_v2(String s) {
+        s = s.trim();
+        String[] arr = s.split("\\s+");
+        return arr[arr.length - 1].length();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
