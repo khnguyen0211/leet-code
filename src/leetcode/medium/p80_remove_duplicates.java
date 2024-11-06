a93ae1ac0f26173cbfdd7985cfd496339bd45524
Khoi Nguyen <pnknguyen0211@gmail.com>
2024-11-06 22:58:05 +0700

    improved p80_remove_duplicates

--- src/leetcode/medium/p80_remove_duplicates.java
+++ src/leetcode/medium/p80_remove_duplicates.java
@@ -4,34 +4,48 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class p80_remove_duplicates {
+    
+    /*
+     * Runtime 0 ms Beats 100%
+     */
+    public static int remove_duplicates(int[] arr) {
+        int j = 2;
+        for (int i = 2; i < arr.length; i++) {
+            if (arr[i] == arr[j - 1] && arr[i] == arr[j - 2]) {
+                continue;
+            }
+            arr[j] = arr[i];
+            j++;
+        }
+        return j;
+    }
+
     /*
      * Runtime 4 ms Beats 5.40%
      */
-    public static int remove_duplicates(int[] arr) {
-        int sum = 0;
+    public static int remove_duplicates_v1(int[] arr) {
+        int count = 0;
         Map<Integer, Integer> map = new HashMap<>();
         for (int i : arr) {
             map.put(i, map.getOrDefault(i, 0) + 1);
         }
-        for (int i = 0; i < arr.length - sum;) {
+        for (int i = 0; i < arr.length - count;) {
             int frequent = map.get(arr[i]);
             if (frequent <= 2) {
                 i += frequent;
                 continue;
             }
             int diff = frequent - 2;
-            sum += diff;
+            count += diff;
             for (int j = i + 2; j < arr.length - diff; j++) {
                 arr[j] = arr[j + diff];
             }
             i += 2;
         }
-        return arr.length - sum;
+        return arr.length - count;
     }
 
     public static void main(String[] args) {
-        // 111223
-        // 112233
         int[] a = new int[] {1, 1, 1, 2, 2, 3};
         System.out.println(remove_duplicates(a));
     }
