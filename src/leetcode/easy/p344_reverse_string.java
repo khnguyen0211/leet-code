d7db0d0978a253d30fc9165fec99ea6f5989ded1
Khoi Nguyen <pnknguyen0211@gmail.com>
2024-11-07 20:39:39 +0700

    solved p344_reverse_string

---
+++ src/leetcode/easy/p344_reverse_string.java
@@ -0,0 +1,19 @@
+package leetcode.easy;
+
+public class p344_reverse_string {
+
+    /*
+     * Runtime 0 ms Beats 100.00%
+     */
+    public static void reverseString(char[] s) {
+        int i = 0;
+        int j = s.length - 1;
+        while (i < j) {
+            char c = s[i];
+            s[i] = s[j];
+            s[j] = c;
+            i++;
+            j--;
+        }
+    }
+}
