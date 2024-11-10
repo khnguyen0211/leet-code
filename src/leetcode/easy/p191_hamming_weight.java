b907abea06af242e0b11a367ea79290d66d455f1
Khoi Nguyen <pnknguyen0211@gmail.com>
2024-11-10 07:29:44 +0700

    solved p191_hamming_weight

---
+++ src/leetcode/easy/p191_hamming_weight.java
@@ -0,0 +1,20 @@
+package leetcode.easy;
+
+public class p191_hamming_weight {
+    /*
+     * Runtime 0 ms Beats 100.00%
+     */
+    public static int hamming_weight(int n) {
+        int count = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((n & (1 << i)) != 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        hamming_weight(11);
+    }
+}
