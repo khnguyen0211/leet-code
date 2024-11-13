87c80b6424fc9f275aaf4df7691b68cc47a4b602
Khoi Nguyen <pnknguyen0211@gmail.com>
2024-11-13 16:43:08 +0700

    solved p2563_count_fair_pairs

---
+++ src/leetcode/medium/p2563_count_fair_pairs.java
@@ -0,0 +1,42 @@
+
+import java.util.Arrays;
+
+
+
+public class p2563_count_fair_pairs {
+    /*
+     * lower_bound Time Limit Exceeded
+     */
+    public static long count_fair_pairs_v1(int[] arr, int lower, int upper) {
+        long count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] >= lower && arr[i] + arr[j] <= upper) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static long count_fair_pairs_v2(int[] arr, int lower, int upper) {
+        Arrays.sort(arr);
+        return lower_bound(arr, upper + 1) - lower_bound(arr, lower);
+    }
+
+    public static long lower_bound(int[] arr, int value) {
+        int left = 0;
+        int right = arr.length - 1;
+        long result = 0;
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (sum < value) {
+                result += right - left;
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return result;
+    }
+}
