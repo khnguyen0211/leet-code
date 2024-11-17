26ff418bf9f3b1777bf69c1b822a1ec71f2ae6e8
Khoi Nguyen <pnknguyen0211@gmail.com>
2024-11-17 23:10:48 +0700

    solved p455_find_content_children

---
+++ src/leetcode/easy/p455_find_content_children.java
@@ -0,0 +1,34 @@
+package leetcode.easy;
+
+import java.util.Arrays;
+
+
+public class p455_find_content_children {
+   
+    /*
+     * Runtime 9 ms Beats 99.68%
+     */
+    public static int find_content_children(int[] g, int[] s) {
+        int count = 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0;
+        int j = 0;
+        while (i < g.length && j < s.length) {
+            while (j < s.length && g[i] > s[j]) {
+                j++;
+            }
+            if (j < s.length) {
+                count++;
+                i++;
+                j++;
+            }
+
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
