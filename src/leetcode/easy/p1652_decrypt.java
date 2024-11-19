0dcc75d0b9f84ec3d438534615897b19ca71c2a1
Khoi Nguyen <pnknguyen0211@gmail.com>
2024-11-19 10:20:31 +0700

    solved p1652_decrypt

---
+++ src/leetcode/easy/p1652_decrypt.java
@@ -0,0 +1,32 @@
+package leetcode.easy;
+
+public class p1652_decrypt {
+    public static int[] decrypt(int[] code, int k) {
+        int i = 0;
+        int n = code.length;
+        int[] result = new int[n];
+        if (k == 0) {
+            return result;
+        }
+        while (i < n) {
+            int sum = 0;
+
+            if (k > 0) {
+                for (int j = i + 1; j <= i + k; j++) {
+                    sum += code[j % n];
+                }
+            } else {
+                for (int j = i - 1; j >= i + k; j--) {
+                    if (j < 0) {
+                        sum += code[n + j];
+                    } else {
+                        sum += code[j];
+                    }
+                }
+            }
+            result[i] = sum;
+            i++;
+        }
+        return result;
+    }
+}
